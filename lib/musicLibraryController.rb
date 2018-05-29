6cf6838c89f001ef99846b32259f1373db171e2f
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-05-29 20:41:13 +0000

    automatically backed up by learn

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -12,6 +12,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets input
+    input = gets.chomp
   end
 end
